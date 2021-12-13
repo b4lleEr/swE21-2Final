d86a0c63c5245ed0a49764a376f4f8de7e930cf1
b4lleEr <80311941+b4lleEr@users.noreply.github.com>
2021-12-13 18:44:41 +0900

    페이지 구현 테스트
    index 페이지에서 각 링크를 클릭해서 개별 페이지에 접근할 수 있도록 구현해봤습니다.

---
+++ index.php
@@ -0,0 +1,49 @@
+    <!DOCTYPE html>
+    <html lang="en" dir="ltr">
+      <head>
+        <meta charset="utf-8">
+        <title></title>
+      </head>
+      <body>
+        <h1><a href="index.php">WEB</h1>
+        <ol>
+        <?php
+        // data 디렉토리의 파일 목록을 출력요청 to PHP
+        // 파일 목록 하나하나를 li와 a 태그를 이용해 목록생성
+        $list = scandir('data');
+        $i = 0;
+        while($i<count($list)){
+            if($list[$i] != '.'){
+                if($list[$i] != '..'){
+                    ?>
+                    <li><a href="index.php?id=<?=$list[$i]?>"><?=$list[$i]?></a></li>
+                    <?php
+                }
+            }
+            $i = $i+1;
+        }
+        ?>
+    </ol>
+            <!-- <ol>
+    <li><a href="index.php?id=HTML">HTML</a></li>
+    <li><a href="index.php?id=CSS">CSS</a></li>
+    <li><a href="index.php?id=JavaScript">JavaScript</a></li>
+    <li><a href="index.php?id=PHP">PHP</a></li>
+  </ol> -->
+    <h2><?php
+        if(isset($_GET['id'])){
+            echo $_GET['id'];
+        } else {
+            echo "Welcome!";
+        }
+       ?>
+      </h2>
+      <?php
+      if(isset($_GET['id'])){
+          echo file_get_contents("data/".$_GET['id']);
+      } else {
+          echo "Hello, PHP";
+      }
+       ?>
+      </body>
+    </html>
